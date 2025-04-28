f91a826886e720c11c3beeeb289a58ed8805318d
dhruvasanani2024 <asanani.dhruv123@gmail.com>
2025-04-28 13:54:57 +0530

    update

--- HMS/settings.py
+++ HMS/settings.py
@@ -27,7 +27,7 @@ SECRET_KEY = config('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = [".vercel.app", "localhost", "192.0.0.1"]
+ALLOWED_HOSTS = [".vercel.app", "localhost", "127.0.0.1"]
 
 
 # Application definition
